9126af8f5a8c98d9765a0452bd823626ca3a03de
François Mockers <mockersf@gmail.com>
2020-02-27 01:09:40 +0100

    :wrench: update data url

--- examples/scatterplot.rs
+++ examples/scatterplot.rs
@@ -2,7 +2,7 @@ use procyon::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let chart = Procyon::chart(
-            "https://raw.githubusercontent.com/davidB/vega_lite_3.rs/master/examples/res/data/clustered_data.csv"
+            "https://raw.githubusercontent.com/procyon-rs/vega_lite_4.rs/master/examples/res/data/clustered_data.csv"
         )
         .mark_point()
         .encode_axis("x", "y").encode_color("cluster")

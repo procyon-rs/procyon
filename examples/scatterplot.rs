a0ffbe39c6844c443a5b6724e30b0d017c27812b
Thomas Aubry <github.thomaub@gmail.com>
2020-04-30 16:57:39 +0200

    :sparkless: use headless firefow with fantocinni to save the plot. only support for firefox and png at the moment

--- examples/scatterplot.rs
+++ examples/scatterplot.rs
@@ -1,15 +1,17 @@
 use procyon::*;
+use showata::Showable;
+use std::path::{Path, PathBuf};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let chart = Procyon::chart(
             "https://raw.githubusercontent.com/procyon-rs/vega_lite_4.rs/master/examples/res/data/clustered_data.csv"
         )
         .mark_point()
         .encode_axis("x", "y").encode_color("cluster")
-        .build();
+        .save(Path::new("/Users/aubrythomas/src/personal-git/procyon-rs/procyon/image.png")).await?;
 
-    eprintln!("{:?}", chart);
-    chart.show().unwrap();
+    //chart.show().unwrap();
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! High level helpers to create beautiful graphs based on Vega-Lite
 
+// warnings,
 #![deny(
-    warnings,
     missing_debug_implementations,
     trivial_casts,
     trivial_numeric_casts,
@@ -13,7 +13,15 @@
 )]
 
 pub use showata::Showable;
-
+//
+use base64;
+use fantoccini::{Client, Locator};
+use retry::{delay::Fixed, retry, OperationResult};
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+use std::{thread, time};
 #[cfg(feature = "vega_lite_4")]
 mod vega_lite_4_bindings;
 
@@ -115,7 +123,86 @@ impl Procyon {
 
         new
     }
+    /// save the image
+    pub async fn save(&self, image_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+        // Spawn in background a webdriver
+        // Currently geckodriver is hardcoded but should support
+        // all webdriver compliant driver.
+        // TODO:updated to all webdriver
+        let mut webdriver_process = Command::new("geckodriver")
+            .args(&["--port", "4444"])
+            .spawn()
+            .expect("webdriver failed to start");
+        // This should also be TODO:updated to all webdriver
+        let mut caps = serde_json::map::Map::new();
+        let opts = serde_json::json!({ "args": ["--headless"]});
+        caps.insert("moz:firefoxOptions".to_string(), opts);
 
+        let mut connection_ok = false;
+        while !connection_ok {
+            thread::sleep(time::Duration::from_secs(5));
+            // This should also be TODO:updated to all webdriver
+            let mut caps = serde_json::map::Map::new();
+            let opts = serde_json::json!({ "args": ["--headless"]});
+            caps.insert("moz:firefoxOptions".to_string(), opts);
+            let mut connection_ok = Client::with_capabilities("http://localhost:4444", caps)
+                .await
+                .is_err();
+            if connection_ok == false {
+                break;
+            };
+        }
+        let mut c = Client::with_capabilities("http://localhost:4444", caps).await?;
+        // let mut c = retry(Fixed::from_millis(100), || {
+        //     match Some(c.as_ref().unwrap()) {
+        //         Some(Client { tx, is_legacy }) => OperationResult::Ok(c),
+        //         _ => OperationResult::Retry("not connected yet"),
+        //     }
+        // })
+        // .unwrap()?;
+
+        //.expect("failed to connect to WebDriver");
+
+        // Current implem use the saved html by showata and imitate user clik to save the image
+        // Another approach is to updated the embeded js like in altair:
+        // https://github.com/altair-viz/altair_saver/blob/master/altair_saver/savers/_selenium.py
+        dbg!(self.build().to_html_file()?.to_str().unwrap());
+        c.goto(&format!(
+            "file:///private{}",
+            self.build().to_html_file()?.to_str().unwrap()
+        ))
+        .await?;
+        // c.goto("file:///private/var/folders/32/h6lt_67s75g6jf3h4hx_myxc0000gn/T/showata/show-1587655243189631000.html").await?;
+        let mut summary_button = c
+            .wait_for_find(Locator::Css("summary"))
+            .await?
+            .click()
+            .await?;
+        let mut hidden_link = summary_button
+            .find(Locator::LinkText("Save as PNG"))
+            .await?
+            .click()
+            .await?;
+
+        let link = hidden_link
+            .find(Locator::LinkText("Save as PNG"))
+            .await?
+            .attr("href")
+            .await?;
+
+        let image_data_url = link.unwrap();
+        //let data64: Vec<&str> = image_data_url.split(',').collect::<Vec<&str>>();
+        //dbg!(data64[1]);
+        //let bytes: Vec<u8> = base64::decode(data64[1]).unwrap();
+        // "png;base64,iVB"
+        let _format = &image_data_url[11..14];
+        let bytes: Vec<u8> = base64::decode(&image_data_url[22..]).unwrap();
+        let mut image_file = File::create(image_path).unwrap();
+        image_file.write(&bytes).unwrap();
+        hidden_link.close().await;
+        webdriver_process.kill();
+        Ok(())
+    }
     /// Build the graph
     #[cfg(feature = "vega_lite_4")]
     pub fn build(&self) -> vega_lite_4::Vegalite {
